32854163f1cfd8ec519c56b79b981e2216de8b00
tatsumack <tatsu.mack@gmail.com>
2018-10-25 08:55:44 +0900

    use go1.11

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tatsumack/atcoder-video-api
+
+require google.golang.org/appengine v1.2.0
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"google.golang.org/appengine"
+	"net/http"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	// if statement redirects all invalid URLs to the root homepage.
+	// Ex: if URL is http://[YOUR_PROJECT_ID].appspot.com/FOO, it will be
+	// redirected to http://[YOUR_PROJECT_ID].appspot.com.
+	if r.URL.Path != "/" {
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+
+	fmt.Fprintln(w, "Hello, Gopher Network!")
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	appengine.Main() // Starts the server to receive requests
+}
